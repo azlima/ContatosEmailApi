4baff99948bea19b766e6701915dd61d348e90eb
azlima <gilbertoluis@terra.com.br>
2017-05-06 15:07:38 -0300

    email textfile.

--- ContatosEmailApi/Controllers/ContatosController.cs
+++ ContatosEmailApi/Controllers/ContatosController.cs
@@ -1,16 +1,12 @@
 ﻿using ContatosEmailApi.Models;
-using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
